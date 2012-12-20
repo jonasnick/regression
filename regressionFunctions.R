14389dd6f73d8a401db5d3b741ec1617ba3ccd2a
jonasnick <jonas-david.nick@student.uni-tuebingen.de>
2012-12-20 17:23:14 +0100

    Bugfix + least squares cost

--- regressionFunctions.R
+++ regressionFunctions.R
@@ -1,3 +1,11 @@
 g <- function(z) {
-return(1/(1+exp(-z)))
+    return(1/(1+exp(-z)))
+}
+
+Jtest <- function(X, y, theta) {
+    #X <- matrix(c(c(1, 2, 3), c(1,2,3), c(1,2,3)), 3)
+    #theta <- c(1, 2, 3)
+    #y <- c(10, 20, 30)
+    m <- length(y)
+    return(sum(((X%*%theta)-y)^2)/(2*m))
 }
